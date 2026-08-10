77ab5410bff353cba6041a0594bbe34a5be95815
Vas <95457459+lovehersins@users.noreply.github.com>
2026-08-10 01:09:57 -0500

    Update discord-auth.js

--- netlify/functions/discord-auth.js
+++ netlify/functions/discord-auth.js
@@ -1,13 +1,17 @@
-exports.handler = async () => {
-  const clientID = process.env.DiscordClient;
- 
-  const redirect =
-  `https://discord.com/oauth2/authorize?client_id=${clientID}&response_type=code&redirect_uri=https://taupe-seahorse-7784d8.netlify.app/.netlify/functions/discord-auth`;
- 
+exports.handler = async function(event) {
+  const clientId = "1477546371665105068";
+  const redirectUri = "https://taupe-seahorse-7784d8.netlify.app/.netlify/functions/discord-callback";
+
+  const discordUrl =
+    `https://discord.com/oauth2/authorize?client_id=${clientId}` +
+    `&response_type=code` +
+    `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+    `&scope=identify%20guilds`;
+
   return {
     statusCode: 302,
     headers: {
-      Location: redirect
+      Location: discordUrl
     }
   };
 };
