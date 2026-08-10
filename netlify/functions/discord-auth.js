ea700c2a0fae821fa50760dc3d045aeac5f65f7b
Vas <95457459+lovehersins@users.noreply.github.com>
2026-08-10 00:58:12 -0500

    Update discord-auth.js

--- netlify/functions/discord-auth.js
+++ netlify/functions/discord-auth.js
@@ -2,7 +2,7 @@ exports.handler = async () => {
   const clientID = process.env.DiscordClient;
  
   const redirect =
-  `https://discord.com/oauth2/authorize?client_id=${clientID}&response_type=code&redirect_uri=https://YOUR-SITE.netlify.app/.netlify/functions/discord-auth&scope=identify`;
+  `https://discord.com/oauth2/authorize?client_id=${clientID}&response_type=code&redirect_uri=https://taupe-seahorse-7784d8.netlify.app/.netlify/functions/discord-auth`;
  
   return {
     statusCode: 302,
